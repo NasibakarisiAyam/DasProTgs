88613b6ed9bf33c894093a6a8ed4b291a092e54d
Atha <garooooxxx@gmail.com>
2026-09-14 10:29:36 +0700

    yaa

--- is_cumlaude.py
+++ is_cumlaude.py
@@ -1,7 +1,7 @@
 # Nama File: is_cumlaude.py
 # Pembuat : Athallah Nazif Arlis Pratama
 # Tanggal : 07 Oktober 2026
-# Deskripsi : Fungsi yang menerima input a:float dan b:float dan mengembalikan nilai true jika ipk >= 3.0 and waktu <= 54 
+# Deskripsi : Fungsi yang menerima input a:float dan b:float dan mengembalikan nilai true jika 3.50<=ipk<=4.0 and waktu <= 54 
 # Definisi dan spesifikasi : 
 #  is_cumlaude(x,waktu): float -> bool
 #   {is_cumlaude(x,y) akan menerima input float dan akan bernilai true jika ipk >= 3.50 and  waktu <= 54}
@@ -10,3 +10,5 @@ def iscumlaude(ipk:float, waktu:float)-> bool:
     return(3.50<=ipk<=4.0 and waktu <= 54)
 
 print(iscumlaude(3.90,54))
+print(iscumlaude(1.0,54))
+print(iscumlaude(4.0,60))
